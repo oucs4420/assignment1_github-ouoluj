cf6fdcb218a6b2b7e26744c7e169ac80fdcff3e0
Derek Baker <db820017@ohio.edu>
2022-01-14 22:26:12 -0500

    Completed assignment

--- filesize.cc
+++ filesize.cc
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-// using namespace std;
 
 // output format to use IDENTICALLY:
 //BSH:Saru> ./filesize input.3lines input.1line 
@@ -15,7 +14,6 @@ int main( int argc, char* argv[] )
     // just to get you started, this is how to refer to the arguments that were passed
     std::cout << "program: " << argv[0] << '\n';
     for (int arg = 1; arg < argc; ++arg) {
-        // std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
         std::cout << " " << argv[arg] << ": " << countLines(argv[arg]) <<'\n';
     }
 
@@ -28,19 +26,17 @@ int countLines(char* fileName) {
     int result = -1;
 
     file.open(fileName);
-    // std::cout << file.fail();
     if (file.fail()) {
         return -1;
     }
 
     std::string dummy = "";
-    while (!file.eof()) {
-        getline(file, dummy);
-        // if (file.eof()) {
-        //     break;
-        // }
-        // std::cout << "AAAAAAA" << '\n'; 
+
+    while (getline(file, dummy)) {
         result++;
+        if (file.peek() == EOF) {
+            result ++;
+        }
     }
 
     file.close();
